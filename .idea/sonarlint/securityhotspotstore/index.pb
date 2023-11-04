5739f0b3c51fd4a20003a7828d559b52cdc5812f
Василий Мешков <vaskyinki@yandex.ru>
2023-11-04 23:55:26 +0300

    V2

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+n
+>src/main/java/com/mvas/client/controller/ClientController.java,6\a\6a2ad1b961b8529867345a13f80e940495b56a96
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Å
+Q.idea/sonarlint/securityhotspotstore/6/a/6a2ad1b961b8529867345a13f80e940495b56a96,4\8\4847adb2384ac463b1935cba63bc03f0abeb30f7
+w
+G.idea/sonarlint/issuestore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,8\6\863e0292fd7e9b3e74dafd6cc01f73ba76346482
+Å
+Q.idea/sonarlint/securityhotspotstore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,2\c\2cabb939da04d3de3a200bf2b575690e1a16316d
--- src/main/java/com/mvas/client/controller/ClientController.java
+++ src/main/java/com/mvas/client/controller/ClientController.java
@@ -5,33 +5,78 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
+
+import javax.crypto.Cipher;
+import java.io.UnsupportedEncodingException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @Controller
 public class ClientController {
 
+    private RSAPublicKey clientPublicKey;
+    private RSAPrivateKey clientPrivateKey;
+
     @PostMapping("/sendMessage")
-    public String sendMessage(@RequestParam("message") String message, Model model) {
-        // Создайте HttpHeaders и установите заголовок "Content-Type"
+    public String sendMessage(@RequestBody String message, Model model) {
+        try {
+            String encryptedMessage = encryptWithServerPublicKey(message);
+
+            String response = sendEncryptedMessageToServer(encryptedMessage);
+
+            model.addAttribute("response", "Сообщение получено: " + response);
+
+        } catch (Exception e) {
+            model.addAttribute("response", "Error: " + e.getMessage());
+        }
+
+        return "result";
+    }
+
+    private String encryptWithServerPublicKey(String message) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        String serverUrl = "http://localhost:8081/getServerPublicKey";
+        String serverPublicKeyBase64 = restTemplate.postForObject(serverUrl, null, String.class);
+
+        RSAPublicKey serverPublicKey = decodeRSAPublicKey(serverPublicKeyBase64);
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+
+        byte[] encryptedBytes = cipher.doFinal(message.getBytes("UTF-8")); // Указываем кодировку UTF-8
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    private RSAPublicKey decodeRSAPublicKey(String publicKeyBase64) throws Exception {
+        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyBase64);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+        return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(keySpec);
+    }
+
+    private String sendEncryptedMessageToServer(String encryptedMessage) {
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setContentType(MediaType.TEXT_PLAIN);
 
-        // Создайте HttpEntity с параметром "message"
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("message", message);
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        HttpEntity<String> request = new HttpEntity<>(encryptedMessage, headers);
 
-        // Отправьте POST-запрос на сервер
         RestTemplate restTemplate = new RestTemplate();
         String serverUrl = "http://localhost:8081/receiveMessage";
         String response = restTemplate.postForObject(serverUrl, request, String.class);
 
-        model.addAttribute("response", "Текст получен " + response);
+        try {
+            response = new String(response.getBytes("ISO-8859-1"), "UTF-8"); // Декодирование из ISO-8859-1 в UTF-8
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
-        return "result";
+        return response;
     }
 }
